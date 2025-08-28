1a35eab3bf74b390cd10943ca4eac87c7e9412ee
渡辺のぞみ <shogo55.kk4@gmail.com>
2025-08-28 22:09:46 +0900

    console.log表示順に並び替え→var SampleArrayBはfunctionのうしろ？　reduceに戻す→for文が6回の実行へ

--- script.js
+++ script.js
@@ -5,16 +5,18 @@ var sampleArrayA = [
     [ 'B1', 'B2', 'B3' ],
     [ 'C1', 'C2', 'C3' ]
 ];
-console.log(2);
-
-var sampleArrayB = sampleArrayA.reverse();
-console.log(3);//
+console.log(2);//var変数定義のため実行順に合わせて前へ。
 
 function toOneDimension(previousValue, currentValue){
-console.log(4);//
-    return previousValue.concat(currentValue)
-    console.log(5);
+console.log(4);//2回
+    return previousValue.concat(currentValue);
+    console.log(5);//実行されず。？？
 }
+var sampleArrayB = sampleArrayA.reduce(toOneDimension);
+console.log(3);//4のあと？？→function関数の後へ
+
+
+
     
 for (var counterVar = 0; counterVar < sampleArrayB.length; counterVar++){
     console.log(6);//
