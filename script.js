e7bada3d9de529ae377805b258c64765812a8d2c
渡辺のぞみ <shogo55.kk4@gmail.com>
2025-09-01 22:02:05 +0900

    順番を戻しfunction{}の後ろへもconsple挿入

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 console.log(1);//1
 
+function toOneDimension(previousValue,　currentValue){
+console.log(2);//2回（5のあと？）
+    return currentValue.concat(previousValue);
+    console.log(3);//単体では実行されず。
+}
+console.log(4);
+
 var sampleArrayA = [
     [ 'A1', 'A2', 'A3' ],
     [ 'B1', 'B2', 'B3' ],
     [ 'C1', 'C2', 'C3' ]
 ];
-console.log(2);//var変数定義のため実行順に合わせて前へ。
+console.log(5);//
 
-function toOneDimension(previousValue, currentValue){
-console.log(4);//2回
-    return currentValue.concat(previousValue);
-    console.log(5);//実行されず。？？
-}
 var sampleArrayB = sampleArrayA.reduce(toOneDimension);
-console.log(3);//4のあと？？→function関数の後へ
-
-
+console.log(6);//2
 
     
 for (var counterVar = 0; counterVar < sampleArrayB.length; counterVar++){
-    console.log(6);//
+    console.log(7);//0‐8の9回
             
     document.write('sampleArrayB['+counterVar+']:');
     document.write(sampleArrayB[counterVar]);
     document.write('<br/>');
 } 
-console.log(7);
+console.log(8);
