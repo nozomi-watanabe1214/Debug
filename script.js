94b65aaf7aa341073ec807885c8f9dd8922caff3
渡辺のぞみ <shogo55.kk4@gmail.com>
2025-09-01 23:02:32 +0900

    変数値console（途中）

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
-console.log(1);
-
+//1
 function toOneDimension(previousValue,　currentValue){
-console.log(2);//4(2回（5のあと？）)
-    return currentValue.concat(previousValue);
-    console.log(3);//単体では実行されず。
-}
-console.log(4);//2
+//4(2回)
+    console.log(previousValue);
+    return previousValue.concat(currentValue);
+}//2
+
 
 var sampleArrayA = [
     [ 'A1', 'A2', 'A3' ],
     [ 'B1', 'B2', 'B3' ],
     [ 'C1', 'C2', 'C3' ]
-];
-console.log(5);//3
+];//3
+console.log(sampleArrayA);
 
 var sampleArrayB = sampleArrayA.reduce(toOneDimension);
-console.log(6);//5
-
-    
+//5
+console.log(sampleArrayB);
 for (var counterVar = 0; counterVar < sampleArrayB.length; counterVar++){
-    console.log(7);//7(0‐8の9回)
-            
+//6(9回)         
     document.write('sampleArrayB['+counterVar+']:');
     document.write(sampleArrayB[counterVar]);
     document.write('<br/>');
-} 
-console.log(8);8
+}//7
