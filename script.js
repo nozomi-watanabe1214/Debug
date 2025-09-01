8aba4712bc13b1a0232ece2127a975136f7665d8
渡辺のぞみ <shogo55.kk4@gmail.com>
2025-09-01 22:44:05 +0900

    関数実行順把握console.log

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-console.log(1);//1
+console.log(1);
 
 function toOneDimension(previousValue,　currentValue){
-console.log(2);//2回（5のあと？）
+console.log(2);//4(2回（5のあと？）)
     return currentValue.concat(previousValue);
     console.log(3);//単体では実行されず。
 }
-console.log(4);
+console.log(4);//2
 
 var sampleArrayA = [
     [ 'A1', 'A2', 'A3' ],
     [ 'B1', 'B2', 'B3' ],
     [ 'C1', 'C2', 'C3' ]
 ];
-console.log(5);//
+console.log(5);//3
 
 var sampleArrayB = sampleArrayA.reduce(toOneDimension);
-console.log(6);//2
+console.log(6);//5
 
     
 for (var counterVar = 0; counterVar < sampleArrayB.length; counterVar++){
-    console.log(7);//0‐8の9回
+    console.log(7);//7(0‐8の9回)
             
     document.write('sampleArrayB['+counterVar+']:');
     document.write(sampleArrayB[counterVar]);
     document.write('<br/>');
 } 
-console.log(8);
+console.log(8);8
